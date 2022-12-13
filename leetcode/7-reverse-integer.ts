a8457178801a9542b91686333f0e0ec1ad5ca0e7
可圭共 <keguigong@gmail.com>
2022-12-13 16:46:33 +0800

    Update README.md

--- leetcode/7-reverse-integer.ts
+++ leetcode/7-reverse-integer.ts
@@ -3,5 +3,16 @@
  */
 
 function reverse(x: number): number {
-  // your code here
+  let num = 0;
+  let x1 = Math.abs(x);
+  while (x1 !== 0) {
+    num = num * 10 + (x1 % 10);
+    x1 = Math.floor(x1 / 10);
+    if (num < -Math.pow(2, 31) || num > Math.pow(2, 31) - 1) return 0;
+  }
+  return x > 0 ? num : -num;
 }
+
+let input = -123;
+// let input = 1534236469;
+console.log(reverse(input));
