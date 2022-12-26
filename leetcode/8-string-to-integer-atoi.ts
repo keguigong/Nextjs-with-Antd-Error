fba8acc878ee98ad8bd422edaed279bf1b55565d
可圭共 <keguigong@gmail.com>
2022-12-26 19:05:42 +0800

    Update README.md

--- leetcode/8-string-to-integer-atoi.ts
+++ leetcode/8-string-to-integer-atoi.ts
@@ -1,50 +1,30 @@
 /**
  * https://leetcode.cn/problems/string-to-integer-atoi/
  */
+
 function myAtoi(s: string): number {
-  let isNegtive = 1;
-  let tempArr = [];
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === " ") continue;
-
-    if (/[a-z]/.test(s[i])) return 0;
-
-    if (s[i] === "+" && i + 1 < s.length && /[0-9]/.test(s[i + 1])) {
-      while (i + 1 < s.length && /[0-9]/.test(s[i + 1])) {
-        tempArr.push(parseInt(s[i + 1]));
-        i += 1;
-      }
-      break;
-    }
-
-    if (s[i] === "-" && i + 1 < s.length && /[0-9]/.test(s[i + 1])) {
-      isNegtive = -1;
-
-      while (i + 1 < s.length && /[0-9]/.test(s[i + 1])) {
-        tempArr.push(parseInt(s[i + 1]));
-        i += 1;
-      }
-      break;
-    }
-
-    while (i < s.length && /[0-9]/.test(s[i])) {
-      tempArr.push(parseInt(s[i]));
-      i += 1;
-    }
-    break;
+  let index = 0;
+  while (index < s.length && s[index] === " ") {
+    index++;
   }
 
-  let prevVal = 0;
-  let calcVal = prevVal;
-  for (let i = 0; i < tempArr.length; i++) {
-    calcVal = prevVal * 10 + tempArr[i];
-    if (calcVal * isNegtive > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
-    else if (calcVal * isNegtive < -Math.pow(2, 31)) return -Math.pow(2, 31);
-    prevVal = calcVal;
+  let sign = 1;
+  if (s[index] === "-") {
+    sign = -1;
+    index++;
+  } else if (s[index] === "+") {
+    index++;
   }
-  return calcVal * isNegtive;
+
+  let calcVal = 0;
+  while (index < s.length && /[0-9]/.test(s[index])) {
+    calcVal = calcVal * 10 + Number.parseInt(s[index]);
+    if (sign * calcVal > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
+    else if (sign * calcVal < -Math.pow(2, 31)) return -Math.pow(2, 31);
+    index++;
+  }
+  return calcVal * sign;
 }
 
-let str = "+-12";
+let str = "-12";
 console.log(myAtoi(str));
