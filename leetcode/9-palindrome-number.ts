b8f335313374e079ca01727ec1ab46efdd202aaa
可圭共 <keguigong@gmail.com>
2022-12-26 22:31:46 +0800

    Update README.md

--- leetcode/9-palindrome-number.ts
+++ leetcode/9-palindrome-number.ts
@@ -4,15 +4,16 @@
 
 function isPalindrome(x: number): boolean {
   if (x < 0) return false
+  if (x > 0 && x % 10 === 0) return false
 
   let initVal = x
   let revVal = 0
-  for(;initVal > 0;) {
+  for(;initVal > revVal;) {
     revVal = revVal * 10 + initVal % 10
     initVal = Math.floor(initVal / 10)
   }
-  return revVal === x
+  return initVal === revVal || initVal === Math.floor(revVal / 10)
 };
 
-let num = 12321
+let num = 10
 console.log(isPalindrome(num))
