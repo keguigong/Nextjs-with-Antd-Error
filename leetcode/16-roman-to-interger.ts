e35d636da0f0e266b52cd493a6f501f14180b0cb
可圭共 <keguigong@gmail.com>
2023-01-09 15:15:41 +0800

    Update README.md

--- leetcode/16-roman-to-interger.ts
+++ leetcode/16-roman-to-interger.ts
@@ -3,5 +3,26 @@
  */
 
 function romanToInt(s: string): number {
-  
+  let romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+
+  for(let i = 0; i < s.length; i++) {
+    if (i + 1 < s.length && romanMap[s[i]] < romanMap[s[i + 1]]) {
+      result -= romanMap[s[i]]
+    } else {
+      result += romanMap[s[i]]
+    }
+  }
+  return result;
 }
+
+let roman = "MCMXCIV";
+console.log(romanToInt(roman));
