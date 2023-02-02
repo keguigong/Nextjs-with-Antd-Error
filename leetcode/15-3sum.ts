81fe59883173394733a8338d2412c84d212d035c
可圭共 <keguigong@gmail.com>
2023-02-02 17:28:06 +0800

    Update README.md

--- leetcode/15-3sum.ts
+++ leetcode/15-3sum.ts
@@ -3,6 +3,39 @@
  * https://leetcode.cn/problems/3sum/
  */
 
+// 双指针
+function threeSum(nums: number[]): number[][] {
+  let ans = [];
+  const len = nums.length;
+  if (nums == null || len < 3) return ans;
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] == nums[i - 1]) continue;
+    let left = i + 1;
+    let right = len - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum == 0) {
+        ans.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] == nums[left + 1]) left++;
+        while (left < right && nums[right] == nums[right - 1]) right--;
+        left++;
+        right--;
+      } else if (sum < 0) left++;
+      else if (sum > 0) right--;
+    }
+  }
+  return ans;
+}
+
+(function () {
+  let nums = [-1, 0, 1, 2, -1, -4];
+  console.log(threeSum(nums));
+})();
+
 // 暴力解法
 function threeSum1(nums: number[]): number[][] {
   let tempList: number[][] = [];
@@ -30,35 +63,3 @@ function threeSum1(nums: number[]): number[][] {
   );
   return numsList;
 }
-
-// 双指针
-function threeSum(nums: number[]): number[][] {
-  let ans = [];
-  const len = nums.length;
-  if (nums == null || len < 3) return ans;
-  nums.sort((a, b) => a - b);
-
-  for (let i = 0; i < len; i++) {
-    if (nums[i] > 0) break;
-    if (i > 0 && nums[i] == nums[i - 1]) continue;
-    let left = i + 1;
-    let right = len - 1;
-    while (left < right) {
-      const sum = nums[i] + nums[left] + nums[right];
-      if (sum == 0) {
-        ans.push([nums[i], nums[left], nums[right]]);
-        while (left < right && nums[left] == nums[left + 1]) left++;
-        while (left < right && nums[right] == nums[right - 1]) right--;
-        left++;
-        right--;
-      } else if (sum < 0) left++;
-      else if (sum > 0) right--;
-    }
-  }
-  return ans;
-}
-
-(function () {
-  let nums = [-1, 0, 1, 2, -1, -4];
-  console.log(threeSum(nums));
-})();
