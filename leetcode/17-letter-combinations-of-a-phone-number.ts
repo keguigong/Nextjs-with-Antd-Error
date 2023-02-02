b79d6f4172e3dbf61c7d16e3f3a142047a73f3a4
可圭共 <keguigong@gmail.com>
2023-02-02 18:17:56 +0800

    Update README.md

---
+++ leetcode/17-letter-combinations-of-a-phone-number.ts
@@ -0,0 +1,39 @@
+/**
+ * 电话号码的字母组合
+ * https://leetcode.cn/problems/letter-combinations-of-a-phone-number/
+ */
+
+function letterCombinations(digits: string): string[] {
+  const keyMap = {
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
+  };
+  let comb = [];
+
+  function backtrace(digits: string, index: number, letters: string) {
+    if (index >= digits.length) {
+      letters && comb.push(letters);
+      return;
+    }
+
+    const key: string[] = keyMap[digits[index]];
+    key.forEach((elememt) => {
+      let _letters = letters + elememt;
+      backtrace(digits, index + 1, _letters);
+    });
+  }
+
+  backtrace(digits, 0, "");
+  return comb;
+}
+
+(function () {
+  let digits = "234";
+  console.log(letterCombinations(digits));
+})();
