be488a0506ef503977e1bfc7c794daaafa90caa0
可圭共 <keguigong@gmail.com>
2023-02-02 18:46:30 +0800

    Update README.md

---
+++ leetcode/26-remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,34 @@
+/**
+ * 删除有序数组中的重复项
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
+ */
+
+function removeDuplicates(nums: number[]): number {
+  let left = 0;
+  const len = nums.length;
+
+  for (let right = 1; right < len; right++) {
+    if (nums[right] != nums[left]) {
+      nums[left + 1] = nums[right];
+      left++;
+    }
+  }
+  return left + 1;
+}
+
+(function () {
+  let nums = [1, 1, 2];
+  console.log(removeDuplicates(nums));
+})();
+
+// 简单做法
+function removeDuplicates1(nums: number[]): number {
+  let index = 0;
+  while (index < nums.length) {
+    while (index + 1 < nums.length && nums[index] === nums[index + 1]) {
+      nums.splice(index, 1);
+    }
+    index++;
+  }
+  return nums.length;
+}
