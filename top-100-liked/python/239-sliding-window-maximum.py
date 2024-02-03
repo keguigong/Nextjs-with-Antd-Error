fefd23cf910f3e22cfba5d504e8f3dcff0debd89
keguigong <keguigong@gmail.com>
2024-02-04 00:06:19 +0800

    239-sliding-window-maximum.py

---
+++ top-100-liked/python/239-sliding-window-maximum.py
@@ -0,0 +1,27 @@
+# 239. 滑动窗口最大值
+# https://leetcode.cn/problems/sliding-window-maximum
+
+import collections
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums or k == 0:
+            return []
+        deque = collections.deque()
+        # 未形成窗口
+        for i in range(k):
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+        res = [deque[0]]
+        # 形成窗口后
+        for i in range(k, len(nums)):
+            if deque[0] == nums[i - k]:
+                deque.popleft()
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+            res.append(deque[0])
+        return res
