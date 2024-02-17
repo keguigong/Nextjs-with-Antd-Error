8e0ad1f010341d912798b32d8dbd96ec8980c23c
keguigong <keguigong@gmail.com>
2024-02-17 19:07:19 +0800

    Update 131-palindrome-partitioning.py

--- top-100-liked/python/131-palindrome-partitioning.py
+++ top-100-liked/python/131-palindrome-partitioning.py
@@ -5,47 +5,25 @@ from typing import List
 
 
 class Solution:
-    def __init__(self):
-        self.ans: List[List[str]] = []
-
     def partition(self, s: str) -> List[List[str]]:
-        def helper(begin: int, substr: List[str]):
-            n = len(s)
-            # print("begin = {}, substr = {}".format(begin, substr))
+        ans = []
 
-            if not begin < n:
-                self.ans.append(substr)
-            else:
-                for i in range(begin, n):
-                    if i == begin:
-                        right = i
-                        while right < n and s[right] == s[begin]:
-                            temp = substr[:]
-                            temp.append(
-                                s[begin : right + 1] if right + 1 < n else s[begin:]
-                            )
-                            helper(right + 1, temp)
-                            right += 1
-                    else:
-                        left, right = i, i
-
-                        while right + 1 < n and s[right + 1] == s[right]:
-                            right += 1
-
-                        while (
-                            left - 1 >= begin
-                            and right + 1 < n
-                            and s[right + 1] == s[left - 1]
-                        ):
-                            right += 1
-                            left -= 1
-
-                        if left == begin:
-                            temp = substr[:]
-                            temp.append(
-                                s[begin : right + 1] if right + 1 < n else s[begin:]
-                            )
-                            helper(right + 1, temp)
-
-        helper(0, [])
-        return self.ans
+        def dfs(i, comb):
+            if i == len(s):
+                ans.append(comb[:])
+                return
+            
+            for j in range(i, len(s)):
+                if isPalindrome(i, j):
+                    dfs(j + 1, comb + [s[i : j + 1]])
+        
+        def isPalindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        dfs(0, [])
+        return ans
