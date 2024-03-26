28bd44a9e5191f18d0cda2c6027e215db7cb7e09
keguigong <keguigong@gmail.com>
2024-03-26 23:35:06 +0800

    48-rotate-image.py

---
+++ top-100-liked/python/48-rotate-image.py
@@ -0,0 +1,22 @@
+# 48. 旋转图像
+# https://leetcode.cn/problems/rotate-image
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range((n + 1) // 2):
+                (
+                    matrix[i][j],
+                    matrix[n - j - 1][i],
+                    matrix[n - i - 1][n - j - 1],
+                    matrix[j][n - i - 1],
+                ) = (
+                    matrix[n - j - 1][i],
+                    matrix[n - i - 1][n - j - 1],
+                    matrix[j][n - i - 1],
+                    matrix[i][j],
+                )
+48-rotate-image.py
