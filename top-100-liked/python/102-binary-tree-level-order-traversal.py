1a67ac0a6bc670ef0311096010a57b4540b7baa5
keguigong <keguigong@gmail.com>
2024-04-08 22:12:15 +0800

    Update 102-binary-tree-level-order-traversal.py

--- top-100-liked/python/102-binary-tree-level-order-traversal.py
+++ top-100-liked/python/102-binary-tree-level-order-traversal.py
@@ -1,6 +1,8 @@
 # 102. 二叉树的层序遍历
 # https://leetcode.cn/problems/binary-tree-level-order-traversal
-from typing import List, Optional
+from collections import deque
+from sys import last_exc
+from typing import Deque, List, Optional
 
 
 # Definition for a binary tree node.
@@ -15,24 +17,22 @@ class Solution:
     def __init__(self) -> None:
         self.ans = []
 
-    def printLevel(self, queue: List[TreeNode]):
-        temp: List[TreeNode] = []
-        ans: List[int] = []
-        for node in queue:
-            ans.append(node.val)
-            if node.left:
-                temp.append(node.left)
-            if node.right:
-                temp.append(node.right)
-        self.ans.append(ans)
-        if len(temp) > 0:
-            self.printLevel(temp)
-
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue: List[TreeNode] = []
         if not root:
             return []
-        else:
-            queue.append(root)
-        self.printLevel(queue)
-        return self.ans
+        queue: Deque[TreeNode] = deque()
+        queue.append(root)
+        ans: List[List[int]] = []
+        while len(queue):
+            n = len(queue)
+            layerList = []
+            for i in range(n):
+                node = queue.popleft()
+                layerList.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ans.append(layerList)
+        return ans
+
