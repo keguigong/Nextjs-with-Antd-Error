7b09e378b8a4cc66d119e56cb00b9d7229212739
keguigong <keguigong@gmail.com>
2024-04-10 00:10:53 +0800

    72-edit-distance.py

---
+++ top-100-liked/python/72-edit-distance.py
@@ -0,0 +1,21 @@
+# 72. 编辑距离
+# https://leetcode.cn/problems/edit-distance/
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(
+                        dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1
+                    )
+        return dp[-1][-1]
